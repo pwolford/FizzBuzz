3614d16bc79eddd5a62ddbeff6feb4489dbab504
pwolford <philwolford17@gmail.com>
2014-11-16 15:06:44 -0500

    FizzBuzz
    Prints from checkFizzBuzz instead of returning a value

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,22 +3,22 @@ public class FizzBuzz {
 public static void main(String[] args) {
     int count = 1;
     while (count <= 100){
-        System.out.println(checkFizzBuzz(count));
+        checkFizzBuzz(count);
         count++;
   }
 }  
-  public static String checkFizzBuzz(int count) {
-    if ((count % 3 == 0) && (count % 5 == 0)) {
-      return "FizzBuzz";
-    } else if (count % 3 == 0) {
-      return "Fizz";
+  public static void checkFizzBuzz(int count) {
+    String displayFizzBuzz = Integer.toString(count);
+      if (count % 3 == 0) {
+      displayFizzBuzz = "Fizz";
     } else if (count % 5 == 0) {
-      return "Buzz";
-    } else {
-      return Integer.toString(count);
-    }
+      displayFizzBuzz += "Buzz";
+    } 
+    System.out.println(displayFizzBuzz);
   }
-  
 }
+
+  
+
 
 
